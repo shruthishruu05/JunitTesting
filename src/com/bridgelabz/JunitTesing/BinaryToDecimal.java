55418c7090105d224e7f4eaacdcef3b5fe6096cc
shruthishruu05 <shruthi.m@perfios.com>
2021-08-24 20:20:51 +0530

    ADDED: BinaryToDecimal Program

--- src/com/bridgelabz/JunitTesing/BinaryToDecimal.java
+++ src/com/bridgelabz/JunitTesing/BinaryToDecimal.java
@@ -2,26 +2,23 @@ package com.bridgelabz.JunitTesing;
 public class BinaryToDecimal {
 		static int binaryToDecimal(int n)
 		{
-			int num = n;
-			int dec_value = 0;
+			int number = n;
+			int decValue = 0;
 			int base = 1;
 
-			int temp = num;
+			int temp = number;
 			while (temp > 0) {
-				int last_digit = temp % 10;
-	          System.out.println("last"+last_digit);
+				int lastDigit = temp % 10;
 				temp = temp / 10;
-
-				dec_value += last_digit * base;
-
+				decValue += lastDigit * base;
 				base = base * 2;
 			}
 
-			return dec_value;
+			return decValue;
 		}
 		public static void main(String[] args)
 		{
-			int num = 10111;
-			System.out.println(binaryToDecimal(num));
+			int number = 10111;
+			System.out.println(binaryToDecimal(number));
 		}
 	}
