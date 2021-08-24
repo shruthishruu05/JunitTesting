16bc70b7416ee8843057fd551f9bbad3ad103c9e
shruthishruu05 <shruthi.m@perfios.com>
2021-08-24 19:38:43 +0530

    Added: Sqrt program

---
+++ src/com/bridgelabz/JunitTesing/Sqrt.java
@@ -0,0 +1,23 @@
+package com.bridgelabz.JunitTesing;
+
+public class Sqrt {
+	
+	public static void Sqrt(double c)
+	{
+		double t =c;
+		double epsilon = 1*Math.E-15;
+		double average = 0.5*((c/t)+t);
+		while(Math.abs(t - c/t) < epsilon*t)
+		{
+			average = Math.abs(t - c/t);
+			
+		}	
+		System.out.println(average);
+	}
+	public static void main(String[] args)
+	{
+		double c = 250;
+		Sqrt(c);
+	}
+
+}
