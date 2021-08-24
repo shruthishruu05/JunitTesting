a6f44b71cc924b4ad40b49a40b9faaa0d69023f6
shruthishruu05 <shruthi.m@perfios.com>
2021-08-24 18:04:41 +0530

    REFACTORED: Vending Machine

--- src/com/bridgelabz/JunitTesing/VendingMachine.java
+++ src/com/bridgelabz/JunitTesing/VendingMachine.java
@@ -5,6 +5,21 @@ public class VendingMachine
 	static int notes[]= {1000,500,50,5,2,1};
 	static int money;
 	static int changeNotes;
+	public static void VendingMachine2(int notes[],int money,int indexVal)
+	{
+		int index = indexVal;
+		int remainder;
+		if(money>0) {
+			changeNotes = money/notes[index];
+			remainder = money%notes[index];
+			money = remainder;
+			if(changeNotes != 0)
+				System.out.println("The " +notes[index]+ " is " +changeNotes);
+			VendingMachine2(notes,money,++indexVal);
+		}
+		else
+			return;
+	}
 	public static void main(String[] args)
 	{
 		
@@ -12,7 +27,7 @@ public class VendingMachine
 		System.out.println("Enter the money to know the number of notes");
 		money = in.nextInt();
 		System.out.println("money entered "+money);
-		VendingMachine(notes,money);
+		VendingMachine2(notes,money,0);
 	}
 	public static void VendingMachine(int notes[],int money)
 	{
@@ -22,10 +37,10 @@ public class VendingMachine
 		{
 			if(money>=notes[index])
 			{
-		changeNotes = money/notes[index];
-		remainder = money%notes[index];
-		money = remainder;
-		System.out.println("The " +notes[index]+ " is " +changeNotes);
+				changeNotes = money/notes[index];
+				remainder = money%notes[index];
+				money = remainder;
+				System.out.println("The " +notes[index]+ " is " +changeNotes);
 			}
 			index++;
 		}	
