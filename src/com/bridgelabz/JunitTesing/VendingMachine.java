0b96dd75b2577f73d2646e0ad56edf6daa6e36ab
shruthishruu05 <shruthi.m@perfios.com>
2021-08-24 18:08:00 +0530

    REFACTORED: Vending Machine

--- src/com/bridgelabz/JunitTesing/VendingMachine.java
+++ src/com/bridgelabz/JunitTesing/VendingMachine.java
@@ -5,7 +5,7 @@ public class VendingMachine
 	static int notes[]= {1000,500,50,5,2,1};
 	static int money;
 	static int changeNotes;
-	public static void VendingMachine2(int notes[],int money,int indexVal)
+	public static void VendingMachine(int notes[],int money,int indexVal)
 	{
 		int index = indexVal;
 		int remainder;
@@ -15,7 +15,7 @@ public class VendingMachine
 			money = remainder;
 			if(changeNotes != 0)
 				System.out.println("The " +notes[index]+ " is " +changeNotes);
-			VendingMachine2(notes,money,++indexVal);
+			VendingMachine(notes,money,++indexVal);
 		}
 		else
 			return;
@@ -27,24 +27,9 @@ public class VendingMachine
 		System.out.println("Enter the money to know the number of notes");
 		money = in.nextInt();
 		System.out.println("money entered "+money);
-		VendingMachine2(notes,money,0);
+		VendingMachine(notes,money,0);
 	}
-	public static void VendingMachine(int notes[],int money)
-	{
-		int index = 0;
-		int remainder;
-		while(money>0)
-		{
-			if(money>=notes[index])
-			{
-				changeNotes = money/notes[index];
-				remainder = money%notes[index];
-				money = remainder;
-				System.out.println("The " +notes[index]+ " is " +changeNotes);
-			}
-			index++;
-		}	
-	}
+	
 }
 		
 		
